4f0e2c4f597287cc2b0ba05509b231074c28a444
Omar-HA-Issa <oissa.ieu2023@student.ie.edu>
2026-01-27 22:08:40 +0100

    Fix Railway deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,6 @@
-console.log("[startup] Node.js starting...");
-console.log("[startup] PORT:", process.env.PORT);
-
 import "dotenv/config";
-
-console.log("[startup] dotenv loaded");
-console.log("[startup] Env check - SUPABASE_URL:", !!process.env.SUPABASE_URL);
-console.log("[startup] Env check - SUPABASE_ANON_KEY:", !!process.env.SUPABASE_ANON_KEY);
-console.log("[startup] Env check - SUPABASE_SERVICE_ROLE_KEY:", !!process.env.SUPABASE_SERVICE_ROLE_KEY);
-console.log("[startup] Env check - OPENAI_API_KEY:", !!process.env.OPENAI_API_KEY);
-
 import app from "./server";
 import { logger } from "./shared/utils/logger";
-
-console.log("[startup] Server module loaded successfully");
 
 const PORT = process.env.PORT || 3001;
 
